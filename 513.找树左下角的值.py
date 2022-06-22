f8327db739be9faf9400eb557d588f39b74ec789
asus-laptop <1536967497@qq.com>
2022-06-22 08:37:18 +0800

    first commit

---
+++ 513.找树左下角的值.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode.cn id=513 lang=python3
+#
+# [513] 找树左下角的值
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+
+
+class Solution:
+    """
+    BFS:使用队列逐层搜索
+    向下搜索么每层时，先搜索右子树，然后搜索左子树，这样才能保证每一层最终留下的是左叶子。
+    """
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            if node.right:
+                q.append(node.right)
+            if node.left:
+                q.append(node.left)
+            ans = node.val
+        return ans
+# @lc code=end
+
