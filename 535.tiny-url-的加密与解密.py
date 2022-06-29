80993d9583cfedfbc5809f9be49313ff5e652248
asus-laptop <1536967497@qq.com>
2022-06-29 09:48:54 +0800

    solve the problem 535

---
+++ 535.tiny-url-的加密与解密.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode.cn id=535 lang=python3
+#
+# [535] TinyURL 的加密与解密
+#
+
+# @lc code=start
+class Codec:
+    def __init__(self) -> None:
+        self.database = {}
+        self.id = 0
+
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        self.id += 1
+        self.database[self.id] = longUrl
+
+        return "http://tinyurl.com/" + str(self.id)
+        
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        i = shortUrl.rfind("/") #返回最后一次出现的位置
+        id = int(shortUrl[i+1:])
+        return self.database[id]
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
+# @lc code=end
+
