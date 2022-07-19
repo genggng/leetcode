60f30ba4725ec5f7d10c2fe4f816dd4bc72a055b
asus-laptop <1536967497@qq.com>
2022-07-19 21:57:19 +0800

    try to solve problem 731

--- 729.我的日程安排表-i.py
+++ 729.我的日程安排表-i.py
@@ -26,6 +26,7 @@ from sortedcontainers import SortedDict
 class MyCalendar:
     """
     方法二：使用排序字典存储日程，能够保证排序结构，同时支持快速插入
+    排序字典会对主键进行排序
     使用二分查找合适的位置降低时间复杂度
     """
     def __init__(self):
@@ -35,9 +36,9 @@ class MyCalendar:
         # bisect_left是找到一个合适的位置插入使得原字典有序
         # all(val < end for val in a[lo:i]) 左侧的都小于end
         # all(val >= end for val in a[i:hi]) 右侧（包含i）的都大于等于end
-        i = self.tasks.bisect_left(end)  #寻找大于end的第一个节点 r1>=end
-        # if r2<=start <ennd <= r1 即可插入
-        if i==0 or self.tasks.items()[i-1][1] <=start:  #寻找前一个区间(l2,r2)，r2<=start
+        i = self.tasks.bisect_left(end)  #寻找大于end的第一个节点 end<=l2
+        # if r1<=start <end <= l1 即可插入
+        if i==0 or self.tasks.items()[i-1][1] <=start:  #寻找前一个区间(l1,r1)，r1<=start
             self.tasks[start] = end
             return True
         return False
---
+++ 731.我的日程安排表-ii.py
@@ -0,0 +1,50 @@
+#
+# @lc app=leetcode.cn id=731 lang=python3
+#
+# [731] 我的日程安排表 II
+#
+
+# @lc code=start
+from sortedcontainers import SortedDict
+class MyCalendarTwo:
+    """
+    这道题相较于I，容许二重日程的存在，而不允许三重日程。
+    只需要将二重日程标记为I中的普通日程即可使用相同的算法。
+    """
+
+    def __init__(self):
+        self.tasks = SortedDict()
+        self.is_merge = dict() #标记是否是二重
+
+    def book(self, start: int, end: int) -> bool:
+        # bisect_left是找到一个合适的位置插入使得原字典有序
+        # all(val < end for val in a[lo:i]) 左侧的都小于end
+        # all(val >= end for val in a[i:hi]) 右侧（包含i）的都大于等于end
+        i = self.tasks.bisect_left(end)  #寻找大于end的第一个节点 end<=l2
+        print(f"task{self.tasks.items()} add ({start,end}) find i={i}")
+        # 考虑不重叠情况，if r1<=start <ennd <= r2 ,即可插入
+        if i==0 or self.tasks.items()[i-1][1] <=start:  #寻找前一个区间(l1,r1)，r1<=start
+            self.tasks[start] = end
+            self.is_merge[start] = False
+            # print("没有重叠")
+            return True
+        # 考虑重叠情况：
+        # (l1,r1) 为双重日程，不行。
+        # (l1,r1) 不为双重日程，可能行。判断是否与(l0,r0)有交集。
+        l1,r1 = self.tasks.items()[i-1]
+        if not self.is_merge[l1] and (i-1 <= 0 or (not self.is_merge[self.tasks.items()[i-2][0]] and\
+            self.tasks.items()[i-2][1]<=start)):
+            s = min(l1,start)
+            if l1 != s: self.tasks.pop(l1) #删除合并前的元素
+            self.tasks[s] = max(r1,end)
+            self.is_merge[s] = True
+            # print("二重折叠")
+            return True
+        return False
+
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
+# @lc code=end
+
