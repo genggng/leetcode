42c75c014acdec5ce83ac5c0ab6cc4685797c7e1
asus-laptop <1536967497@qq.com>
2022-08-22 21:13:09 +0800

    copy solution 655 on leetcode

---
+++ 655.输出二叉树.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode.cn id=655 lang=python3
+#
+# [655] 输出二叉树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def printTree(self, root: Optional[TreeNode]) -> List[List[str]]:
+        def calDepth(node: Optional[TreeNode]) -> int:
+            return max(calDepth(node.left) + 1 if node.left else 0, calDepth(node.right) + 1 if node.right else 0)
+        height = calDepth(root)
+
+        m = height + 1
+        n = 2 ** m - 1
+        ans = [[''] * n for _ in range(m)]
+        def dfs(node: Optional[TreeNode], r: int, c: int) -> None:
+            ans[r][c] = str(node.val)
+            if node.left:
+                dfs(node.left, r + 1, c - 2 ** (height - r - 1))
+            if node.right:
+                dfs(node.right, r + 1, c + 2 ** (height - r - 1))
+        dfs(root, 0, (n - 1) // 2)
+        return ans
+
+# @lc code=end
+
