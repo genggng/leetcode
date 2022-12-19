c26aa1acbeee3166a140f0bff8f380d5aabe0e61
asus-laptop <1536967497@qq.com>
2022-12-19 17:42:14 +0800

    solution 1764

---
+++ 1764.通过连接另一个数组的子数组得到一个数组.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=1764 lang=python3
+#
+# [1764] 通过连接另一个数组的子数组得到一个数组
+#
+
+# @lc code=start
+class Solution:
+    def canChoose(self, groups: List[List[int]], nums: List[int]) -> bool:
+        
+# @lc code=end
+
